3225db0c172791cd21a4298bf78daa3688ad35bc
Abubakar Usman <abusmoh@gmail.com>
2017-11-25 00:40:53 -0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import { Navbar, Jumbotron, Button } from 'react-bootstrap';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Hello, welcome to my UTube!</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
